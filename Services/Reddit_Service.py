f4b9cfae6823c38e34042d26140ea8cce1d62200
Maxwell Sime <maxwellsime@gmail.com>
2024-07-15 20:36:56 +0100

    Fix importing parallel folder module

--- Services/Reddit_Service.py
+++ Services/Reddit_Service.py
@@ -1,5 +1,8 @@
-from Automatic_TTS_Video_Generator.Routes.Reddit_API import get_top_posts
+import sys
+sys.path.append('.')
+from Routes.Reddit_API import get_top_posts
 
 def get_top_greentext_posts():
     posts = get_top_posts('greentext')
+    print(posts.text)
     
